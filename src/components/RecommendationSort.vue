5e859fd18aa2d704b41cf768a445c84bdc0f6525
文谦 肖 <1846565780@qq.com>
2024-03-14 16:08:34 +0800

    v0.0.3

--- src/components/RecommendationSort.vue
+++ src/components/RecommendationSort.vue
@@ -15,7 +15,7 @@
           <option v-for = "number in numbers" :key = "number" :value = "number">{{ number }}</option>
         </select>
       </p>
-        <ul>
+        <ul style="list-style: none;">
             <li v-for="(item, index) in topItems" :key="index">
               {{ index + 1 }}. {{ item.nameSight }} --- 浏览量: {{ item.views }} --- 评分：{{ item.score }}
             </li>
@@ -38,51 +38,68 @@ export default {
       limitedNumber: '10',
       numbers: [5, 10, 15, 20, 50],
       // json
-      items: []
+      items: [],
+      topItems: []
     }
   },
   created () {
-    axios.get('@/public/SightsInfo.json')
-      .then(Response => {
-        this.items = Response.data
+    axios.get('/static/json/SightsInfo.json')
+      .then(response => {
+        this.items = response.data
+        this.updateTopItems()
       })
-      .catch(Error => {
-        console.log(Error)
+      .catch(error => {
+        console.log(error)
       })
   },
-  computed: {
-    topItems () {
+  watch: {
+    selectedType: 'updateTopItems',
+    searchTerm: 'updateTopItems',
+    limitedNumber: 'updateTopItems'
+  },
+  methods: {
+    updateTopItems () {
       let heap = new MinHeap()
-      this.items.forEach(item => {
+      let a1 = 0.1
+      let a2 = 0.9
+      // console.log(heap.heap[0])
+      // console.clear()
+      // let i = 1
+      this.items.item.forEach(item => {
         // 逐个元素插入，保证堆的元素不大于10
         // 加入筛选标签（可拓展）
         // 加入搜索判定
-        if ((this.selectedType === 'all' || item.type === this.selectedType) && item.nameSight.includes(this.searchTerm)) {
+        // console.log('循环次数：' + i)
+        item.combinedScore = a1 * item.views + a2 * item.score
+        // 按照浏览量和用户评分的加权平均排序
+        if ((this.selectedType === 'all' || item.type === this.selectedType) && this.kmpSearch(item.nameSight, this.searchTerm) !== -1) {
           heap.insert(item)
         }
         if (heap.size() > this.limitedNumber) {
           heap.extractMin()
         }
+        // i++
       })
       // 然后再排序！
-      this.quickSort(heap.heap, 0, heap.heap.length - 1)
-      return heap.heap
-    }
-  },
-  methods: {
+      this.topItems = this.quickSort(heap.heap, 0, heap.heap.length - 1)
+      console.log(JSON.parse(JSON.stringify(heap.heap)))
+    },
+    // .sort((a, b) => b.views - a.views)
     // 快速排序
     quickSort (items, left, right) {
+      // console.log(JSON.parse(JSON.stringify(items)))
       if (left < right) {
         let pivotIndex = this.partition(items, left, right)
         this.quickSort(items, left, pivotIndex - 1)
         this.quickSort(items, pivotIndex + 1, right)
       }
+      return items
     },
     partition (items, left, right) {
-      let pivot = items[right].views
+      let pivot = items[right].combinedScore
       let i = left
       for (let j = left; j < right; j++) {
-        if (items[j].views > pivot) {
+        if (items[j].combinedScore > pivot) {
           [items[i], items[j]] = [items[j], items[i]]
           i++
         }
@@ -91,7 +108,7 @@ export default {
       return i
     },
     // KMP
-    kmpResearch (text, pattern) {
+    kmpSearch (text, pattern) {
       if (pattern.length === 0) {
         return 0
       }
@@ -132,8 +149,9 @@ export default {
 }
 class MinHeap {
   // 最小堆实现不通过完全排序提取排名前十的元素
-  constructor (heap) {
+  constructor () {
     this.heap = []
+    // console.log(JSON.parse(JSON.stringify(this.heap)))
   }
   insert (item) {
     this.heap.push(item)
@@ -143,7 +161,7 @@ class MinHeap {
   bubbleUp (index) {
     while (index > 0) {
       let parentIndex = Math.floor((index - 1) / 2)
-      if (this.heap[parentIndex] && this.heap[parentIndex].views <= this.heap[index].views) {
+      if (this.heap[parentIndex] && this.heap[parentIndex].combinedScore <= this.heap[index].combinedScore) {
         break
       }
       [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]]
@@ -152,6 +170,9 @@ class MinHeap {
   }
   // 提取最小值，就是堆顶
   extractMin () {
+    if (this.heap.length <= 1) {
+      return
+    }
     const min = this.heap[0]
     const end = this.heap.pop()
     if (this.heap.length > 0) {
@@ -170,13 +191,13 @@ class MinHeap {
       let swapIndex = null
       // 决定哪个孩子翻上来
       if (leftChildIndex < length) {
-        if (this.heap[leftChildIndex].views < this.heap[index].views) {
+        if (this.heap[leftChildIndex].combinedScore < this.heap[index].combinedScore) {
           swapIndex = leftChildIndex
         }
       }
       if (rightChildIndex < length) {
         // 额外判定右孩子和左孩子的关系
-        if ((swapIndex === null && this.heap[rightChildIndex].views < this.heap[index].views) || (swapIndex !== null && this.heap[rightChildIndex].views < this.heap[swapIndex].views)) {
+        if ((swapIndex === null && this.heap[rightChildIndex].combinedScore < this.heap[index].combinedScore) || (swapIndex !== null && this.heap[rightChildIndex].combinedScore < this.heap[swapIndex].combinedScore)) {
           swapIndex = rightChildIndex
         }
       }
@@ -191,5 +212,8 @@ class MinHeap {
     return this.heap.length
   }
 }
-
+// todo list:
+// 1. 更好的搜索，模糊搜索
+// 2. 更好的推荐算法（个人兴趣、评价）
+// 3. 分页推流
 </script>
